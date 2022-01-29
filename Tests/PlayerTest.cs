3a4c611d2a0715f70658bf5b6fbb460be389516e
Dominic Meddick <dominicmeddick@gmail.com>
2022-01-29 19:10:08 +0000

    Added NUnit Tests project and initial test for Player.

---
+++ Tests/PlayerTest.cs
@@ -0,0 +1,47 @@
+using System;
+using NUnit.Framework;
+using Repository.Entities;
+
+namespace Tests
+{
+    public class PlayerTest
+    {
+        [Test]
+        public void ConstructPlayerWithRequiredValues()
+        {
+            int nationalityId = 1;
+            DateTime birthday = new DateTime(1994, 12, 4);
+            Player player = new Player("Dominic", "Meddick", nationalityId, birthday);
+
+            Assert.NotNull(player);
+            Assert.That(player.FirstName, Is.EqualTo("Dominic"));
+            Assert.That(player.LastName, Is.EqualTo("Meddick"));
+            Assert.That(player.NationalityId, Is.EqualTo(nationalityId));
+            Assert.That(player.BirthDate, Is.EqualTo(birthday));
+        }
+
+        [Test]
+        public void ConstructPlayerWithoutRequiredValues()
+        {
+            int nationalityId = 1;
+            DateTime birthday = new DateTime(1994, 12, 4);
+
+            Assert.Throws<ArgumentException>
+            (
+                () => new Player(null, "Meddick", nationalityId, birthday)
+            );
+        }
+
+        [Test]
+        public void ConstructTooYoungPlayer()
+        {
+            int nationalityId = 1;
+            DateTime birthday = new DateTime(2006, 12, 4);
+
+            Assert.Throws<ArgumentException>
+            (
+                () => new Player("Dom", "Meddick", nationalityId, birthday)
+            );
+        }
+    }
+}
